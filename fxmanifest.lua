2101101a0be2fb0b10e2abf502698bb2b208285e
Froot Loops <94587874+FrootLoopsBowl@users.noreply.github.com>
2022-01-01 15:52:30 -0500

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'adamant'
+game 'gta5'
+
+client_scripts {
+    "RageUI/RMenu.lua",
+    "RageUI/menu/RageUI.lua",
+    "RageUI/menu/Menu.lua",
+    "RageUI/menu/MenuController.lua",
+    "RageUI/components/*.lua",
+    "RageUI/menu/elements/*.lua",
+    "RageUI/menu/items/*.lua",
+    "RageUI/menu/panels/*.lua",
+    "RageUI/menu/windows/*.lua"
+
+}
+
+server_scripts {
+    'server/server.lua'
+}
+
+client_scripts {
+    'client/function.lua',
+    'client/menu.lua'
+}
