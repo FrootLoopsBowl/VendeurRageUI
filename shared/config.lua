64347c6780724eafc8efb9ca52a8869c89c10bfb
Froot Loops <94587874+FrootLoopsBowl@users.noreply.github.com>
2022-03-06 11:24:29 -0500

    Add files via upload

---
+++ shared/config.lua
@@ -0,0 +1,10 @@
+Config = {
+    VendeurPos = vector3(200.98, 2442.17, 60.44),
+    WeaponsList = {
+        {Label = 'Pistolet', Name = 'weapon_pistol', Price = 50000}, 
+        {Label = 'Pétoire', Name = 'weapon_snspistol', Price = 30000}    
+    },
+    ItemsList = {
+        {Label = 'Kit Neon', Name = 'kitneon', Price = 100}
+    }
+}
